15190a898ca4fcdc387c5ba18e9607675e86f63c
Paul BARDEUR <paul.bardeur@epitech.eu>
2025-02-08 17:49:00 +0100

    feat(Game): Some levels

---
+++ Assets/Rotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    public float rotationSpeed = 100f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(45f, transform.eulerAngles.y + rotationSpeed * Time.deltaTime, 0f);
+    }
+}
